59921958a2f22f1b07ee865c4a26ba246e7e06bb
Jozef Cipa <cipa.jozef@gmail.com>
2017-02-23 21:34:20 +0100

    presentation page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,14 +11,30 @@
 </head>
 <body>
 	<main>
-		<h1>Moja Zastávka</h1>
-		<h3>Už čoskoro...</h3>
-		<div>
-			Viac informácií na 
-			<a href="mailto://cipajozef@gmail.com?subject=Čo+je+MojaZastávka+?">cipajozef@gmail.com</a>
-		</div>
+		<section class="screen">
+			<img src="/dist/screen.png" alt="">
+		</section>
+
+		<section class="description">
+			<h1>Moja Zastávka</h1>
+			<p>
+				Ocitli ste sa v novom meste a chcete navštíviť nejaké miesto, budovu, alebo napríklad obchodné centrum?<br>
+				Čo ak je Váš cieľ na druhej strane mesta?<br>
+				Pravdepodobne zvolíte verejnú dopravu, kde však vystúpit?<br>
+				Ktorá zastávka je tá Vaša?<br>
+				S toutu aplikáciou to už viac nie je Váš problém
+			</p>
+			{{-- <div class="download">
+				app store button
+			</div> --}}
+		</section>
 	</main>
+	<footer>
+		<p>
+			&copy;&nbsp;2017 <a href="https://jozefcipa.com">Jozef Cipa</a> &amp; <a href="http://bart.sk">bart.sk</a>
+		</p>
+	</footer>
 {{-- google analytics --}}
- <script src=""></script>
+ {{-- <script src=""></script> --}}
 </body>
 </html>
